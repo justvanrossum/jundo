07eb87b40a6887b9403d6c23a2b6ce7b431f50ac
justvanrossum <justvanrossum@gmail.com>
2018-12-22 08:56:44 +0100

    working setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name="jundo",
+    description="A general purpose library to help implement undo.",
+    author="Just van Rossum",
+    author_email="justvanrossum@gmail.com",
+    url="https://github.com/justvanrossum/jundo",
+    license="MIT",
+    package_dir={"": "Lib"},
+    packages=find_packages("Lib"),
+    install_requires=[
+        "dataclasses",
+    ],
+    extras_require={
+        "testing": [
+            "pytest",
+        ],
+    },
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing :: Fonts",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
